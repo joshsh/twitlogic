775308e275f4ee61a10cac1f6863a8e185e7a0bb
Joshua Shinavier <josh@fortytwo.net>
2009-09-09 04:06:00 -0400

    Cleanup.

--- src/main/java/net/fortytwo/twitlogic/twitter/TwitterSecurity.java
+++ src/main/java/net/fortytwo/twitlogic/twitter/TwitterSecurity.java
@@ -235,7 +235,7 @@ public class TwitterSecurity {
         setAgent(request);
 
         consumer.sign(request);
-        request.setHeader("Authorization", new String(Base64.encodeBase64("joshsh:escher".getBytes())));
+        request.setHeader("Authorization", new String(Base64.encodeBase64("username:password".getBytes())));
 
         HttpClient client = createClient();
         HttpResponse response = client.execute(request);
