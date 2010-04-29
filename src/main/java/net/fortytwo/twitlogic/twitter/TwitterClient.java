c36cd68b60354e25111c90dfbaf598e69e00e49b
Joshua Shinavier <josh@fortytwo.net>
2010-04-29 13:46:01 -0400

    Another title tweak.

--- src/main/java/net/fortytwo/twitlogic/twitter/TwitterClient.java
+++ src/main/java/net/fortytwo/twitlogic/twitter/TwitterClient.java
@@ -27,7 +27,6 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.LinkedList;
