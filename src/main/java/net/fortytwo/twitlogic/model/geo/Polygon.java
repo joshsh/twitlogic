b0d626ab76827cb00e525f1d87a08249fa7df2c9
Joshua Shinavier <josh@fortytwo.net>
2010-08-21 15:31:58 -0700

    Account for trivial Place polygons, which do occur in Twitter (instead of using a 'point' type, Twitter uses a polygon with four identical vertices).

--- src/main/java/net/fortytwo/twitlogic/model/geo/Polygon.java
+++ src/main/java/net/fortytwo/twitlogic/model/geo/Polygon.java
@@ -8,7 +8,7 @@ import org.json.JSONException;
  * A planar polygon (which approximates a small spherical polygon reasonably well)
  * <p/>
  * Source: http://www.cs.princeton.edu/introcs/35purple/Polygon.java.html
- * 
+ * <p/>
  * User: josh
  * Date: Aug 20, 2010
  * Time: 7:57:58 PM
@@ -35,12 +35,13 @@ public class Polygon {
         this.points[N] = points[0];
     }
 
-    private Polygon(Point[] points) {
+    /*
+    public Polygon(Point[] points) {
         N = points.length;
         this.points = new Point[N + 1];
         System.arraycopy(points, 0, this.points, 0, N);
         this.points[N] = points[0];
-    }
+    }*/
 
     private double signedArea() {
         double sum = 0.0;
@@ -51,26 +52,46 @@ public class Polygon {
     }
 
     public Point findCentroid() {
-        double cx = 0.0, cy = 0.0;
-        for (int i = 0; i < N; i++) {
-            cx = cx + (points[i].getLongitude() + points[i + 1].getLongitude()) * (points[i].getLatitude() * points[i + 1].getLongitude() - points[i].getLongitude() * points[i + 1].getLatitude());
-            cy = cy + (points[i].getLatitude() + points[i + 1].getLatitude()) * (points[i].getLatitude() * points[i + 1].getLongitude() - points[i].getLongitude() * points[i + 1].getLatitude());
+        double a = signedArea();
+
+        // Account for trivial cases, which do occur in Twitter.
+        if (0 == a) {
+            return points[0];
+        } else {
+            double cx = 0.0, cy = 0.0;
+            for (int i = 0; i < N; i++) {
+                cx = cx + (points[i].getLongitude() + points[i + 1].getLongitude()) * (points[i].getLatitude() * points[i + 1].getLongitude() - points[i].getLongitude() * points[i + 1].getLatitude());
+                cy = cy + (points[i].getLatitude() + points[i + 1].getLatitude()) * (points[i].getLatitude() * points[i + 1].getLongitude() - points[i].getLongitude() * points[i + 1].getLatitude());
+            }
+            cx /= (6 * -a);
+            cy /= (6 * -a);
+            return new Point(cx, cy);
         }
-        cx /= (6 * -signedArea());
-        cy /= (6 * -signedArea());
-        return new Point(cx, cy);
     }
 
     public static void main(final String[] args) {
+        /*
+        double[][][] d = new double[][][] {
+                {{0, 0}, {0, 1}, {1, 1}, {1, 0}},
+                {{0, 0}, {1, 0}, {1, 1}, {0, 1}},
+                {{1, 1}, {1, 0}, {0, 0}, {0, 1}},
+                {{0, 1}, {0, 1}, {1, 0}, {0, 0}}
+        };
+
+        for (double[][] ds : d) {
+
+        }
+
         Point[][] polys = new Point[][]{
                 {new Point(0,0), new Point(0, 1), new Point(1, 1), new Point(1, 0)},
                 {new Point(0,0), new Point(1, 0), new Point(1, 1), new Point(0, 1)},
                 {new Point(1,1), new Point(1, 0), new Point(0, 0), new Point(0, 1)},
-                {new Point(0,1), new Point(1, 1), new Point(1, 0), new Point(0, 0)}
+                {new Point(0,1), new Point(1, 1), new Point(1, 0), new Point(0, 0)},
+                {new Point(0,0), new Point(0, 0), new Point(0, 0), new Point(0, 0)}
         };
 
         for (Point[] ps : polys) {
             System.out.println(new Polygon(ps).findCentroid());
-        }
+        }  */
     }
 }
---
+++ src/test/java/net/fortytwo/twitlogic/util/PolygonTest.java
@@ -0,0 +1,35 @@
+package net.fortytwo.twitlogic.util;
+
+import junit.framework.TestCase;
+import net.fortytwo.twitlogic.model.geo.Point;
+import net.fortytwo.twitlogic.model.geo.Polygon;
+import org.json.JSONArray;
+
+/**
+ * User: josh
+ * Date: Aug 21, 2010
+ * Time: 3:15:30 PM
+ */
+public class PolygonTest extends TestCase {
+
+    public void testDirectionInvariance() throws Exception {
+        assertCentroid(0.5, 0.5, "[[0,0], [0,1], [1,1], [1,0]]");
+        assertCentroid(0.5, 0.5, "[[0,0], [1,0], [1,1], [0,1]]");
+        assertCentroid(0.5, 0.5, "[[0,1], [1,1], [1,0], [0,0]]");
+        assertCentroid(0.5, 0.5, "[[0,1], [1,1], [1,0], [0,0]]");
+    }
+
+    public void testTrivialPolygon() throws Exception {
+        assertCentroid(0.0, 0.0, "[[0,0], [1,1]]");        
+        assertCentroid(0.0, 0.0, "[[0,0], [0,0], [0,0], [0,0]]");
+    }
+
+    private void assertCentroid(final double lon,
+                                final double lat,
+                                final String json) throws Exception {
+        Polygon p = new Polygon(new JSONArray(json));
+        Point c = p.findCentroid();
+        assertEquals(lon, c.getLongitude());
+        assertEquals(lat, c.getLatitude());
+    }
+}
