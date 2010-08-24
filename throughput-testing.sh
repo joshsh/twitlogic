abd4524283f87bafee6e0a56cd69feb69725f641
Joshua Shinavier <josh@fortytwo.net>
2010-08-23 20:19:13 -0700

    Adding the throughput testing startup script... but I'll have to organize this preponderance of scripts at somepoint.

---
+++ throughput-testing.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Path to JAR
+JAR=`dirname $0`/target/twitlogic-*-full.jar
+
+# Find Java
+if [ "$JAVA_HOME" = "" ] ; then
+        JAVA="java"
+else
+        JAVA="$JAVA_HOME/bin/java"
+fi
+
+# Set Java options
+if [ "$JAVA_OPTIONS" = "" ] ; then
+        JAVA_OPTIONS="-Xms32M -Xmx512M -XX:+HeapDumpOnOutOfMemoryError"
+fi
+
+# Launch the application
+$JAVA $JAVA_OPTIONS -cp $JAR net.fortytwo.twitlogic.util.ThroughputTesting $*
+
+# Return the program's exit code
+exit $?
+
