90eb6f14fc11e2a966e1152088db1a8d03fa5fa9
Joshua Shinavier <josh@fortytwo.net>
2010-09-13 01:30:46 +0800

    Tweak

--- src/main/java/net/fortytwo/twitlogic/util/TweetMetrics.java
+++ src/main/java/net/fortytwo/twitlogic/util/TweetMetrics.java
@@ -23,7 +23,6 @@ import org.openrdf.sail.memory.MemoryStore;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -149,6 +148,8 @@ sum(g[,2])/length(g[,2])
             int size = bytes.length;
             int zipSize, gzipSize, lzmaSize, minilzoSize;
 
+            System.out.println(new String(bytes));
+            /*
             try {
                 gzipSize = Compression.compress(bytes, Compression.Algorithm.GZIP).length;
                 lzmaSize = Compression.compress(bytes, Compression.Algorithm.LZMA).length;
@@ -159,6 +160,7 @@ sum(g[,2])/length(g[,2])
             }
 
             System.out.println("+\t" + size + "\t" + gzipSize + "\t" + lzmaSize + "\t" + minilzoSize + "\t" + zipSize);
+            */
         }
     }
 }
